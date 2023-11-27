bd18bba2a27356b7874b97f8a1cc1e89cd2fc762
Tykki <judas@Tykkis-MacBook-Pro.local>
2023-11-27 13:18:27 -0600

    link logo to home view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,9 @@ import { HomeComponent } from './home/home.component';
   template: `
   <main>
     <header class="brand-name">
+    <a routerLink="">
       <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+    </a>
     </header>
     <section class="content">
 		  <router-outlet />
